5f12a69cf1ccd0d9164b588b87ea385437bd45c1
Étienne <1372556@cegepmontpetit.ca>
2023-10-24 21:48:42 -0400

    FCT_V1_0__CreationTables
    Création des tables

---
+++ 1372556_ProjetFinal/Sql_Scripts/V1_0__CreationTables.sql
@@ -0,0 +1,45 @@
+-- Utilisation de la base de données nouvellement créée
+USE TP1_Pokemon;
+GO
+
+-- Création de la table Type
+CREATE TABLE Types (
+    idType INT IDENTITY(1,1) PRIMARY KEY,
+    nomType VARCHAR(255) NOT NULL UNIQUE
+);
+
+-- Création de la table Generation
+CREATE TABLE Generation (
+    idGeneration INT IDENTITY(1,1) PRIMARY KEY,
+    numero INT NOT NULL,
+    nom VARCHAR(255) NOT NULL,
+    nombreJeux INT NOT NULL CHECK (nombreJeux >= 0)
+);
+
+-- Création de la table Pokemon
+CREATE TABLE Pokemon (
+    idPokemon INT IDENTITY(1,1) PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL,
+    niveau INT CHECK (niveau >= 0),
+    idGeneration INT,
+    CONSTRAINT fk_Generation FOREIGN KEY (idGeneration) REFERENCES Generation(idGeneration),
+    niveauParDefaut INT DEFAULT 0 
+);
+
+-- Création de la table Dresseur
+CREATE TABLE Dresseur (
+    idDresseur INT IDENTITY(1,1) PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL,
+    prenom VARCHAR(255) NOT NULL,
+    badgeCount INT CHECK (badgeCount >= 0)
+);
+
+-- Création de la table Jeux
+CREATE TABLE Jeux (
+    idJeux INT IDENTITY(1,1) PRIMARY KEY,
+    nomJeu VARCHAR(255) NOT NULL,
+    dateSortie DATE,
+    age INT,
+    idGeneration INT,
+    CONSTRAINT fk_Generation_jeux FOREIGN KEY (idGeneration) REFERENCES Generation(idGeneration)
+);
